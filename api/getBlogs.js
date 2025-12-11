4e1c869dd610b8c94ce35b564b6ceed46dcbd22c
mwasif666 <mwasif66625426@gmail.com>
2025-12-10 19:54:56 -0800

    error fixed

--- api/getBlogs.js
+++ api/getBlogs.js
@@ -3,13 +3,18 @@ import connect from "../lib/mongoose.js";
 import Blog from "../models/projectModels.js";
 
 export default async function handler(req, res) {
-  await connect();
+  try {
+    await connect(); // Connect to MongoDB
 
-  if (req.method === "GET") {
-    const blogs = await Blog.find().sort({ createdAt: -1 });
-    return res.status(200).json(blogs);
+    if (req.method === "GET") {
+      const blogs = await Blog.find().sort({ createdAt: -1 });
+      return res.status(200).json(blogs);
+    }
+
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).end("Method Not Allowed");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
   }
-
-  res.setHeader("Allow", ["GET"]);
-  return res.status(405).end("Method Not Allowed");
 }
